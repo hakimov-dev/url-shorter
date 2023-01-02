05a5f0a36ee68bf7e57dc99062181102e87789dc
Muhammadamin <webhacker0100@gmail.com>
2023-01-02 17:25:33 +0500

    Disable eslint and some updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <h1>Hello world!</h1>
+  <div class="mt-[15%] flex justify-center">
+   <div class="custom-card flex items-center justify-center border-2 border-gray-400 rounded h-auto w-[30%] p-[10px] text-center">
+    <a-input-search
+      v-model:value="value"
+      placeholder="input search text"
+      enter-button="Search"
+      size="large"
+    />
+   </div>
   </div>
 </template>
 
 <script setup>
+const value = ''
 </script>
