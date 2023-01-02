f31c5458153790150b0b449bcf2564c409499271
Muhammadamin <webhacker0100@gmail.com>
2023-01-02 16:10:41 +0500

    Update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'url-shorter',
+    title: 'URl Shorter',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
