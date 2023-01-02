c24ad80532dab6630e819a5a19522f5ecdddac03
Muhammadamin <webhacker0100@gmail.com>
2023-01-02 16:12:23 +0500

    Update file name

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   css: ['ant-design-vue/dist/antd.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/antd-ui'],
+  plugins: ['@/plugins/index'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
