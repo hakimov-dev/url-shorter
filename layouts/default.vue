62509a0cf48bb7dfc9203ff19b2e734ba35986c2
Muhammadamin <webhacker0100@gmail.com>
2023-01-02 16:45:19 +0500

    Added new layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>What's up?</h1>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'default-layout'
+}
+</script>
