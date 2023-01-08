d4fe2d2c4cec10b8056505da7936de9997984cb1
Muhammadamin <webhacker0100@gmail.com>
2023-01-08 19:48:17 +0500

    Deleted nuxt/axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,17 +30,9 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.NUXT_ENV_BASE_URL | '/',
-  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
